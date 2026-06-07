5c16f0f1ad073dca82418f8564508453d77e05ee
Sebastian337 <aleshka_demidov@mail.ru>
2026-06-07 19:27:16 +0300

    Final work

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+import datetime
+from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import pandas as pd
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+def get_year_string(age):
+    if age % 10 == 1 and age % 100 != 11:
+        return "год"
+    elif 2 <= age % 10 <= 4 and not (12 <= age % 100 <= 14):
+        return "года"
+    else:
+        return "лет"
+
+
+year_founded = 1920
+current_year = datetime.datetime.now().year
+age = current_year - year_founded
+winery_age_text = f"{age} {get_year_string(age)}"
+
+excel_data = pd.read_excel("wine.xlsx", keep_default_na=False)
+excel_data.columns = excel_data.columns.str.strip()
+wines = excel_data.to_dict(orient="records")
+
+grouped_wines = defaultdict(list)
+for wine in wines:
+    category = wine.get("Категория", "Без категории")
+    grouped_wines[category].append(wine)
+
+env = Environment(
+    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+)
+template = env.get_template("template.html")
+
+rendered_page = template.render(wines=grouped_wines, winery_age=winery_age_text)
+
+with open("index.html", "w", encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
